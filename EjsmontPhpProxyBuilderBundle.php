f3d4102727befa7bee46e9c32d93a88a8826939a
aejsmont <ejsmont.artur@gmail.com>
2013-03-23 22:15:45 +1100

    initial checking

---
+++ EjsmontPhpProxyBuilderBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ejsmont\PhpProxyBuilderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EjsmontPhpProxyBuilderBundle extends Bundle {
+    
+}
---
+++ Tests/Unit/JunkTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ejsmont\PhpProxyBuilderBundle\Tests\Unit;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Ejsmont\PhpProxyBuilderBundle\DependencyInjection\EjsmontPhpProxyBuilderExtension;
+use Ejsmont\PhpProxyBuilderBundle\EjsmontPhpProxyBuilderBundle;
+
+class DoctrineCacheAdapterTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    public function setup() {
+        parent::setUp();
+        $this->container = new ContainerBuilder();
+        $this->container->setParameter("kernel.cache_dir", dirname(__FILE__) . "/../../build/cache");
+    }
+
+    public function testBundle() {
+        $inst = new EjsmontPhpProxyBuilderBundle();
+        $this->assertTrue(!empty($inst));
+    }
+
+    public function testServiceXml() {
+        $inst = new EjsmontPhpProxyBuilderExtension();
+        $this->assertEquals('ejsmont_php_proxy_builder', $inst->getAlias());
+    }
+
+    public function testLoadApcExtension() {
+        $this->markTestIncomplete();
+        
+        $inst = new EjsmontPhpProxyBuilderExtension();
+        $inst->load(array(), $this->container);
+        $cb = $this->container->get('apcCircuitBreaker');
+
+        $this->assertTrue($cb instanceof CircuitBreaker);
+    }
+
+}
+
